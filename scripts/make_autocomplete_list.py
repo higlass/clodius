edde75bb4a7549b62b6710b12ddbacdb7173f9d6
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-05-26 17:02:37 -0400

    Added script to generate an autocomplete list

--- scripts/make_autocomplete_list.py
+++ scripts/make_autocomplete_list.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python
 
 import collections as col
+import fpark
 import json
 import os
 import os.path as op
 import sys
 import argparse
-
-def save_autcomplete_list(substr_dict, output_dir):
-    '''
-    Save the substring dictionary as a bunch of json files in
-    output_dir
-    '''
-    if not op.exists(output_dir):
-        os.makedirs(output_dir)
-
-    for substr in substr_dict:
-        # the 'ac' in the filename stands for 'autocomplete' and it's
-        # added so that we can have a suggestions without for length 0
-        # substrings (i.e. the when the user hasn't typed anything in yet)
-        out_filename = op.join(output_dir, 'ac_' + substr + '.json')
-        print >>sys.stderr, "substr:", substr, "out_filename:", out_filename
-        with open(out_filename, 'w') as f:
-            json.dump(substr_dict[substr], f)
+import elasticsearch as elastic
 
 def make_autocomplete_list(entries, options):
     '''
@@ -33,17 +18,17 @@ def make_autocomplete_list(entries, options):
                     the tiles should be created
     :return: A list of tiles
     '''
-    # group each entry in json_file according to its starting letter
+    # group each entry in input file according to its starting letter
     ngrams = []
-    longest_name = max(map(len, entries))
-
     substrs = col.defaultdict(list)
 
-    for entry in entries:
+    def entry_to_substrs(entry):
+        substrs = []
+
         if options.name not in entry:
             # if an entry doesn't have a name field, print a warning and continue
             #print >>sys.stderr, "Found entry without a name:", entry['uid']
-            continue
+            return []
         # for each entry get each substring and add the entry to the list
         # of entries containing that substring
         # these lists will then be pruned down to create autocomplete suggestions
@@ -54,18 +39,29 @@ def make_autocomplete_list(entries, options):
                 # make the substrings file and token friendly
                 substr = substr.replace('/', ' ').lower()
                 substr = ' '.join(substr.split()).replace(' ', '_')
-                print >>sys.stderr, "substr:", i, size, substr
 
-                substrs[substr] += [entry]
+                substrs += [((substr), [entry])]
+        return substrs
 
-    for key in substrs:
+    def reduce_substrs(substrs1, substrs2):
         if options.reverse_importance:
-            substrs[key] = sorted(substrs[key], key=lambda x: -float(x[options.importance]))[:options.max_entries_per_autocomplete]
+            return sorted(substrs1 + substrs2, key=lambda x: -float(x[options.importance]))[:options.max_entries_per_autocomplete]
         else:
-            substrs[key] = sorted(substrs[key], key=lambda x: float(x[options.importance]))[:options.max_entries_per_autocomplete]
+            return sorted(substrs1 + substrs2, key=lambda x: float(x[options.importance]))[:options.max_entries_per_autocomplete]
 
-    return substrs
-    # recurse further down
+    substr_entries = entries.flatMap(entry_to_substrs)
+
+    reduced_substr_entries = substr_entries.reduceByKey(reduce_substrs)
+
+    ess = elastic.Elasticsearch(options.elasticsearch_nodes.split(','))
+
+    def save_substr_entry((substr_key, substr_value)):
+        ess.index(options.elasticsearch_index,
+                  options.elasticsearch_doctype,
+                  body = {"suggestions": substr_value},
+                  id = substr_key)
+
+    reduced_substr_entries.foreach(save_substr_entry)
 
 def main():
     parser = argparse.ArgumentParser(description="""
@@ -76,7 +72,7 @@ def main():
     of all the n-grams found in the ski area names.
 """)
 
-    parser.add_argument('json_file', nargs=1)
+    parser.add_argument('input_file', nargs=1)
     parser.add_argument('-n', '--name', default='name',
             help='The field in the json entry which specifies its name')
     parser.add_argument('-i', '--importance', default='importance',
@@ -90,22 +86,31 @@ def main():
             action='store_true',
             help='Use the reverse sorting of the importance value to gauge \
                   the worth of individual entries')
-    parser.add_argument('-o', '--output-dir', 
-            help="The directory into which to place the output files",
-            required=True)
+    parser.add_argument('-c', '--column-names', 
+            help="The column names for the input tsv file",
+            default=None)
 
-    #parser.add_argument('-o', '--options', default='yo',
-    #					 help="Some option", type='str')
-    #parser.add_argument('-u', '--useless', action='store_true', 
-    #					 help='Another useless option')
+    parser.add_argument('--elasticsearch-nodes', 
+            help='Specify elasticsearch nodes to push the completions to',
+            default='localhost:9200')
+    parser.add_argument('--elasticsearch-index',
+            help="The index to place the results in",
+            default='test')
+    parser.add_argument('--elasticsearch-doctype',
+            help="The type of document to index",
+            default="autocomplete")
 
     args = parser.parse_args()
 
-    with open(args.json_file[0], 'r') as f:
-        json_file = json.load(f)
+    dataFile = fpark.FakeSparkContext.textFile(args.input_file[0])
 
-    tiles = make_autocomplete_list(json_file, args)
-    save_autcomplete_list(tiles, args.output_dir)
+    if args.column_names is not None:
+        args.column_names = args.column_names.split(',')
+
+    dataFile = (dataFile.map(lambda x: x.split())
+                        .map(lambda x: dict(zip(args.column_names,x))))
+
+    tiles = make_autocomplete_list(dataFile, args)
 
 if __name__ == '__main__':
     main()
