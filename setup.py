01757754525faa01308d4b87bf545cc77851aa13
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-11-24 21:39:37 -0500

    New setup attempt

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ from Cython.Build import cythonize
 import numpy 
 
 setup(name='clodius',
-    ext_modules=cythonize([Extension("clodius.fast", ["fast.pyx"])]),
      include_dirs=[numpy.get_include()],
       version='0.2.4',
       description='Tile generation of big data',
@@ -14,8 +13,10 @@ setup(name='clodius',
       author_email='pkerpedjiev@gmail.com',
       url='',
       packages=['clodius'],
-      install_requires=[
-                    'cython',
-                    'numpy' ],
+      setup_requires[
+          'cython>=0.x'
+          ],
+      extensions = [Extension("*", ["*.pyx"])],
+      cmdclass={'build_ext': Cython.Build.build_ext},
       scripts=['scripts/make_single_threaded_tiles.py']
      )
