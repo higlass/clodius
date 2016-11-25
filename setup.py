657876de0b3d14c39345a004f21a23765d6413d5
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-11-24 21:17:58 -0500

    Added cythonize

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from Cython.Build import cythonize
 import numpy 
 
 setup(name='clodius',
-    ext_modules=[Extension("clodius.fast", ["clodius/fast.pyx"])],
+    ext_modules=cythonize([Extension("clodius.fast", ["clodius/fast.pyx"])]),
      include_dirs=[numpy.get_include()],
       version='0.2.2',
       description='Tile generation of big data',
