0807b1039df7ac5afb2515ca507f6d5e056e54e9
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-11-24 21:04:41 -0500

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import numpy
 setup(name='clodius',
     ext_modules=cythonize("clodius/fast.pyx"),
      include_dirs=[numpy.get_include()],
-      version='0.2.0',
+      version='0.2.1',
       description='Tile generation of big data',
       author='Peter Kerpedjiev',
       author_email='pkerpedjiev@gmail.com',
