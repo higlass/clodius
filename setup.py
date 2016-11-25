68583ee64c8d02e6a2e75d7d41bd20e8dc7d63df
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-11-24 21:22:19 -0500

    Don't include directory for fast.pyx

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from Cython.Build import cythonize
 import numpy 
 
 setup(name='clodius',
-    ext_modules=cythonize([Extension("clodius.fast", ["clodius/fast.pyx"])]),
+    ext_modules=cythonize([Extension("clodius.fast", ["fast.pyx"])]),
      include_dirs=[numpy.get_include()],
       version='0.2.3',
       description='Tile generation of big data',
