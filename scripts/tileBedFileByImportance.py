4ede88b1dfed6b4bdac076750322e9e70bc85a63
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-12-19 18:18:58 -0500

    Working on bedfile tiling

--- scripts/tileBedFileByImportance.py
+++ scripts/tileBedFileByImportance.py
@@ -2,10 +2,30 @@
 
 from __future__ import print_function
 
+import clodius.fpark as cf
+import h5py
+import math
 import negspy.coordinates as nc
+import numpy as np
+import os
+import os.path as op
 import pybedtools as pbt
 import sys
 import argparse
+
+# all entries are broked up into ((tile_pos), [entry]) tuples
+# we just need to reduce the tiles so that no tile contains more than
+# max_entries_per_tile entries
+# (notice that [entry] is an array), this format will be important when
+# reducing to the most important values
+def reduce_values_by_importance(entry1, entry2, max_entries_per_tile=100, reverse_importance=False):
+    if reverse_importance:
+        combined_entries = sorted(entry1 + entry2,
+                key=lambda x: -float(x[-1]))
+    else:
+        combined_entries = sorted(entry1 + entry2,
+                key=lambda x: float(x[-1]))
+    return combined_entries[:max_entries_per_tile]
 
 def main():
     parser = argparse.ArgumentParser(description="""
@@ -20,6 +40,8 @@ def main():
     parser.add_argument('--assembly', type=str, default='hg19',
             help='The genome assembly to use')
     parser.add_argument('--max-per-tile', type=int, default=100)
+    parser.add_argument('--tile-size', default=1024)
+    parser.add_argument('-o', '--output-file', default='/tmp/tmp.hdf5')
 
     #parser.add_argument('argument', nargs=1)
     #parser.add_argument('-o', '--options', default='yo',
@@ -29,9 +51,47 @@ def main():
 
     args = parser.parse_args()
 
+    if op.exists(args.output_file):
+        os.remove(args.output_file)
+
+    # The tileset will be stored as an hdf5 file
+    print("Writing to: {}".format(args.output_file), file=sys.stderr)
+    f = h5py.File(args.output_file, 'w')
+
+    # We neeed chromosome information as well as the assembly size to properly
+    # tile this data
+    tile_size = args.tile_size
+    chrom_info = nc.get_chrominfo(args.assembly)
+    assembly_size = chrom_info.total_length
+    max_zoom = int(math.ceil(math.log(assembly_size / tile_size) / math.log(2)))
+
+
+    # store some meta data
+    d = f.create_dataset('meta', (1,), dtype='f')
+
+    d.attrs['zoom-step'] = 1            # we'll store data for every zoom level
+    d.attrs['max-length'] = assembly_size
+    d.attrs['assembly'] = args.assembly
+    d.attrs['chrom-names'] = nc.get_chromorder(args.assembly)
+    d.attrs['chrom-sizes'] = nc.get_chromsizes(args.assembly)
+    d.attrs['chrom-order'] = nc.get_chromorder(args.assembly)
+    d.attrs['tile-size'] = tile_size
+    d.attrs['max-zoom'] = max_zoom =  math.ceil(math.log(d.attrs['max-length'] / tile_size) / math.log(2))
+    d.attrs['max-width'] = tile_size * 2 ** max_zoom
+
     bed_file = pbt.BedTool(args.bedfile)
 
-    for line in bed_file:
+
+    print("max_zoom:", max_zoom)
+
+    all_parts = []
+
+    def line_to_np_array(line):
+        '''
+        Convert a bed file line to a numpy array which can later
+        be used as an entry in an h5py file.
+        '''
+
         if args.importance_column is None:
             importance = line.stop - line.start
         else:
@@ -39,8 +99,42 @@ def main():
 
         genome_start = nc.chr_pos_to_genome_pos(str(line.chrom), line.start, args.assembly)
         genome_end = nc.chr_pos_to_genome_pos(line.chrom, line.stop, args.assembly)
-        print("genome_start:", genome_start, "genome_end:", genome_end, "importance:", importance)
+        parts = [genome_start, genome_end] + map(str,line.fields[3:]) + [importance]
 
+        return parts
+
+
+    dset = sorted([line_to_np_array(line) for line in bed_file], key=lambda x: x[0])
+    #dset = [line_to_np_array(line) for line in bed_file]
+
+    '''
+    for i in range(len(dset[0])):
+        col = [d[i] for d in dset]
+        f.create_dataset('{}_{}'.format(max_zoom, i), data=col, compression='gzip')
+    '''
+    f.create_dataset('{}'.format(max_zoom), data=dset, compression='gzip')
+
+    curr_zoom = max_zoom - 1
+
+    tile_width = tile_size
+    pdset = cf.ParallelData(dset).map(lambda x: (int(x[0] / tile_width), x))
+
+    print('pd:', pdset.take(10))
+
+
+    while curr_zoom >= 0:
+        print("x1:", pdset.take(2))
+        pdset = pdset.reduceByKey(lambda e1,e2: reduce_values_by_importance(e1, e2, 
+            max_entries_per_tile = args.max_per_tile))
+        #tile_nums_values = [(int(d[0] / tile_size), d) for d in dset]
+        pdset = pdset.map(lambda x: (x[0] / 2, x))
+
+        print("zd:", pdset.take(5))
+        curr_zoom -= 1
+        
+
+
+    #print("dset:", dset)
     
 
 if __name__ == '__main__':
