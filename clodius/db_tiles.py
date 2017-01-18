bdaba41ad0ad113eca461f786f5ecdcb60df9db1
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-01-17 20:48:19 -0500

    Added 2d tileset info

--- clodius/db_tiles.py
+++ clodius/db_tiles.py
@@ -21,6 +21,27 @@ def get_tileset_info(db_file):
 
     return tileset_info
 
+def get_2d_tileset_info(db_file):
+    conn = sqlite3.connect(db_file)
+    c = conn.cursor()
+
+    row = c.execute("SELECT * from tileset_info").fetchone();
+    tileset_info = {
+            'zoom_step': row[0],
+            'max_length': row[1],
+            'assembly': row[2],
+            'chrom_names': row[3],
+            'chrom_sizes': row[4],
+            'tile_size': row[5],
+            'max_zoom': row[6],
+            'max_width': row[7],
+            "min_pos": [1,1],
+            "max_pos": [row[1], row[1]]
+            }
+    conn.close()
+
+    return tileset_info
+
 def get_tile(db_file, zoom, tile_x_pos):
     tileset_info = get_tileset_info(db_file)
 
