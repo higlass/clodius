25ead4fce7ab81571014c06aaf19f0484dbbf60f
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-03-21 14:48:27 -0400

    Added clodius command line tools

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 from distutils.extension import Extension
 
 from Cython.Build import cythonize
 
 
 import numpy
+print("packages:", find_packages())
 
 setup(
     name='clodius',
@@ -13,10 +15,15 @@ setup(
         "clodius.fast", ["clodius/fast.pyx"], include_dirs=[
             numpy.get_include()])]),
     version='0.3.5',
-    description='Tile generation of big data',
+    description='Tile generation for big data',
     author='Peter Kerpedjiev',
     author_email='pkerpedjiev@gmail.com',
     url='',
-    packages=['clodius'],
-    scripts=['scripts/make_single_threaded_tiles.py', 'scripts/tile_bigWig.py']
+    packages=['clodius', 'clodius.cli'],
+    install_requires=['Click'],
+    entry_points={
+        'console_scripts': [
+            'clodius = clodius.cli.aggregate:cli',
+            ]
+        }
 )
