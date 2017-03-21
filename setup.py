fdaa3460b62a74166cda3d25d4256e81408709cb
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-03-21 17:30:12 -0400

    Bumped the version number

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     name='clodius',
     include_dirs= lazy_cythonize(numpy_include),
     ext_modules = lazy_cythonize(extensions),
-    version='0.4.0',
+    version='0.4.1',
     description='Tile generation for big data',
     author='Peter Kerpedjiev',
     author_email='pkerpedjiev@gmail.com',
