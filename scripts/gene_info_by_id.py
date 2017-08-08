1678c6fd8bd38fd38442fafbb854744a0e638094
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-08-08 10:17:15 -0400

    Added a script to fetch gene summaries from entrez gene

---
+++ scripts/gene_info_by_id.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+import datetime as dt
+import os.path as op
+import re
+import sys
+import requests
+import argparse
+import xml.etree.ElementTree as ET
+
+def main():
+    usage = """
+    python gene_info_by_id.py
+
+    Retrieve gene information from Entrez gene given the gene id. For now this
+    just retrieves the gene summary.
+    """
+    num_args= 0
+    parser = argparse.ArgumentParser(usage=usage)
+
+    parser.add_argument('gene_ids', nargs='*')
+    #parser.add_option('-o', '--options', dest='some_option', default='yo', help="Place holder for a real option", type='str')
+    #parser.add_option('-u', '--useless', dest='uselesss', default=False, action='store_true', help='Another useless option')
+    parser.add_argument('-o', '--output-file', default=None, help="The file to store the downloaded gene information to")
+    parser.add_argument('-f', '--id-list', default=None, help="A file containing a list of ids to retrieve information for")
+
+    args = parser.parse_args()
+
+    if len(args.gene_ids) < 1 and args.id_list is None:
+        # no ids passed as argument and no id list file passed in
+        parser.print_help()
+        sys.exit(1)
+
+    gene_ids = args.gene_ids
+    if args.id_list is not None:
+        with open(args.id_list, 'r') as f:
+            for line in f:
+                try:
+                    gene_id = int(line.strip().split()[0])
+                    gene_ids += [gene_id]
+                except Exception as ex:
+                    continue
+
+    for gene_id in gene_ids:
+        try:
+            gene_id = int(gene_id)
+        except Exception as ex:
+            continue
+
+        link = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=gene&id={}&retmode=xml".format(gene_id)
+        ret = requests.get(link)
+
+        tree = ET.fromstring(ret.content)
+        entrez_gene = tree.find('Entrezgene')
+        
+        #gene_type = entrez_gene.find('Entrezgene_type').get('value')
+        #gene_desc = entrez_gene.find('Entrezgene_gene').find('Gene-ref').find('Gene-ref_desc').text
+        try:
+            gene_summary = entrez_gene.find('Entrezgene_summary').text
+            print("{}\t{}".format(gene_id, gene_summary))
+        except Exception as ex:
+            print(ex, "gene_id:", gene_id, "ret_status:", ret.status_code, file=sys.stderr )
+            print("{}\tNo description found".format(gene_id))
+         
+
+if __name__ == '__main__':
+    main()
+
