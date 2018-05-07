c9eb57c216226b00cb0ce1abc5d107215a306e7a
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-05-07 14:49:23 -0400

    Bump version: 0.8.0 → 0.9.0

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     name='clodius',
     include_dirs= lazy_cythonize(numpy_include),
     ext_modules = lazy_cythonize(extensions),
-    version='0.8.0',
+    version='0.9.0',
     description='Tile generation for big data',
     author='Peter Kerpedjiev',
     author_email='pkerpedjiev@gmail.com',
