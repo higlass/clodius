ae1ce86f5c916d1262b5e220dee7264b8102c12b
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2019-01-19 08:25:37 -0800

    Changed version to v0.10.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ install_requires = [
 
 setup(
     name='clodius',
-    version='0.10.0.rc4',
+    version='0.10.0',
     description='Tile generation for big data',
     author='Peter Kerpedjiev',
     author_email='pkerpedjiev@gmail.com',
