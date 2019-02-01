b422892314a2aaad88db19250484f5f750963550
Burak Han Alver <burak_alver@hms.harvard.edu>
2019-02-01 09:41:03 -0500

    bed file with states and/or variable binsizes to multivec (#46)
    * Deleted line that had remained after testing
    
    * added function to break down lines to match the base resolution
    
    * Removed line that had remained from a previous testing
    
    * Revert "Removed line that had remained from a previous testing"
    
    This reverts commit 282479f6904107d7417aaffe07b21bde244caab8.
    
    * Revert "added function to break down lines to match the base resolution"
    
    This reverts commit a82206f876160e6918d3859830e02d97e6be2f1c.
    
    * Removed a buggy print statement
    
    * Updated bumpversion.cfg
    
    * Bump version: 0.10.0 → 0.10.1
    
    * Added lines to deal with binsizes != to base_resolution
    
    * Revert "Added lines to deal with binsizes != to base_resolution"
    
    This reverts commit b5b3a02f0f7dd3843a226c8c77ccc3dacae044d7.
    
    * Added lines to deal with binsizes != resolution
    
    * fixed if statement in warning message
    
    * Added function to deal with states categorical data
    
    * fixed syntax error
    
    * added row_infos to the main function
    
    * added row_infos to the function in the convert.py file
    
    * remove decode( 'iutf8')
    
    * fixed syntax error
    
    * modifed states_to_multivec function
    
    -Cleaned up the code that creates the states_array
    -took out the states_dictionary out of the function so that it is only created once
    -Added an assertion to check if the row_file was provided.
    
    * modified assertion messages
    
    * added print statement for testing
    
    * added print statement for testing
    
    * added  print statement for testing
    
    * decoding the strings in bytes
    
    -Decoding the string in bytes to be compatible with the rows_info file
    
    * Editting the way the bedline is being formatted
    
    * breakdown mapping to dictionary
    
    * uncommented line
    
    * modified str that makes dictionary
    
    * fixed line
    
    * Removed testing print statements and comments
    
    * added line removed by accident
    
    * fixed the warning to check for start coordinate also
    
    * Added a message in the format option explaining the format for each choice
    
    * Added line in the CHANGELOG describing recent changes
    
    * Uploaded test files for format= states
    
    * rewrote the CHANGELOG file to be more informative
    
    * Fixed states_bedline_to_vector function to work with binary input
    
    * modified the docstring of the states_bedline_to_vector function
    
    - Added more examples and descriptions to make ir more informative
    
    * Added test for the states format option
    
    * Fixed syntax error
    
    * fixed syntax error 2

--- test/multivec_test.py
+++ test/multivec_test.py
@@ -37,3 +37,29 @@ def test_bedfile_to_multivec():
 
 def test_load_multivec_tiles():
     input_file = op.join(testdir, 'sample_data', 'sample.bed.multires.mv5')
+
+def test_states_format_befile_to_multivec():
+    runner = clt.CliRunner()
+    input_file = op.join(testdir, 'sample_data', 'states_format_input_testfile.bed.gz')
+    rows_info_file = op.join(testdir,'sample_data', 'states_format_test_row_infos.txt')
+    f = tempfile.NamedTemporaryFile(delete=False)
+    # print("input_file", input_file)
+
+    result = runner.invoke(
+            ccc.bedfile_to_multivec,
+            [input_file,
+                '--format', 'states',
+                '--row-infos-filename', rows_info_file,
+                '--assembly', 'hg38',
+                '--starting-resolution' , '200',
+                '--num-rows', '10'])
+
+    import traceback
+    a,b,tb = result.exc_info
+    '''
+    print("exc_info:", result.exc_info)
+    print("result:", result)
+    print("result.output", result.output)
+    print("result.error", traceback.print_tb(tb))
+    print("Exception:", a,b)
+    '''
