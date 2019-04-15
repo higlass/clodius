41883c074e4d195389be0743b0893af2b95441bc
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-04-15 11:39:55 -0400

    split monolithic test

--- test/mrmatrix_test.py
+++ test/mrmatrix_test.py
@@ -28,10 +28,7 @@ class TilesetInfoTest(unittest.TestCase):
         self.tileset_max = AttrDict(tileset_stub)
         self.tileset_max.attrs = {'max-pos': (9, 9)}
 
-    def test_tileset_info(self):
-        # TODO: "bounds" kwarg is is not actually used... Is something missing?
-
-        # Assertions:
+    def test_default(self):
         default = {
             'bins_per_dimension': 256,
             'max_pos': (2, 2),
@@ -42,6 +39,7 @@ class TilesetInfoTest(unittest.TestCase):
         self.assertEqual(tileset_info(self.tileset), default)
         self.assertEqual(tileset_info(self.tileset, bounds='???'), default)
 
+    def test_with_min(self):
         with_min = {
             'bins_per_dimension': 256,
             'max_pos': (2, 2),
@@ -52,6 +50,7 @@ class TilesetInfoTest(unittest.TestCase):
         self.assertEqual(tileset_info(self.tileset_min), with_min)
         self.assertEqual(tileset_info(self.tileset_min, bounds='???'), with_min)
 
+    def test_with_max(self):
         with_max = {
             'bins_per_dimension': 256,
             'max_pos': (9, 9),
@@ -61,6 +60,7 @@ class TilesetInfoTest(unittest.TestCase):
         }
         self.assertEqual(tileset_info(self.tileset_max), with_max)
         self.assertEqual(tileset_info(self.tileset_max, bounds='???'), with_max)
+
 
 class TilesTest(unittest.TestCase):
     def test_tiles(self):
