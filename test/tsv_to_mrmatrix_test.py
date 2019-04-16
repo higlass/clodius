cf4abbfc7f94340b7fdb0d0e8a95577f1b2b5c1b
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-04-16 11:34:46 -0400

    Test aggregation math... though the results seem weird to me.

--- test/tsv_to_mrmatrix_test.py
+++ test/tsv_to_mrmatrix_test.py
@@ -7,7 +7,7 @@ import h5py
 from scripts.tsv_to_mrmatrix import coarsen, parse
 
 class CoarsenTest(unittest.TestCase):
-    def test_something(self):
+    def test_5_layer_pyramid(self):
         tile_size = 4
         max_zoom = 4
         max_width = tile_size * 2 ** max_zoom
@@ -52,3 +52,48 @@ class CoarsenTest(unittest.TestCase):
                 hdf5['resolutions']['16']['values'][:].tolist(),
                 [row, row, row, row])
             # TODO: Check the math
+
+    def test_math(self):
+        tile_size = 2
+        max_zoom = 2
+        max_width = tile_size * 2 ** max_zoom
+
+        with TemporaryDirectory() as tmp_dir:
+            hdf5 = h5py.File(tmp_dir + '/temp.hdf5', 'w')
+            g = hdf5.create_group('resolutions')
+            g1 = g.create_group('1')
+            ds = g1.create_dataset('values', (max_width, max_width),
+                    dtype='f4', compression='lzf', fillvalue=np.nan)
+            for y in range(max_width):
+                a = np.array([float(x) for x in range(max_width)])
+                ds[y, :max_width] = a
+
+            coarsen(hdf5, tile_size=tile_size)
+
+            # after coarsen()
+            self.assertEqual(list(hdf5.keys()), ['resolutions'])
+            self.assertEqual(list(hdf5['resolutions'].keys()), ['1', '2', '4'])
+
+            shapes = {
+                '1': 8,
+                '2': 4,
+                '4': 2
+            }
+            for (k, v) in shapes.items():
+                self.assertEqual(hdf5['resolutions'][k]['values'].shape, (v, v))
+
+            row8 = list(range(8))
+            self.assertEqual(
+                hdf5['resolutions']['1']['values'][:].tolist(),
+                [row8 for _ in range(8)])
+
+            # TODO: I'll look at the code, but these don't feel right to me.
+            row4 = [8 * x + 2 for x in range(4)]
+            self.assertEqual(
+                hdf5['resolutions']['2']['values'][:].tolist(),
+                [row4 for _ in range(4)])
+
+            row2 = [24, 88]
+            self.assertEqual(
+                hdf5['resolutions']['4']['values'][:].tolist(),
+                [row2 for _ in range(2)])
