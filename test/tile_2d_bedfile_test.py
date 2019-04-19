dfba3aebaa22435dec11729ee7ec3716a2ed329b
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-04-19 13:32:01 -0400

    Fix whitespace on tests that have not been touched in a year

--- test/tile_2d_bedfile_test.py
+++ test/tile_2d_bedfile_test.py
@@ -4,6 +4,7 @@ import clodius.db_tiles as cdt
 import h5py
 import sqlite3
 
+
 def test_get_tileset_info():
     filename = 'test/sample_data/arrowhead_domains_short.txt.multires.db'
     t = cdt.get_tileset_info(filename)
@@ -13,9 +14,10 @@ def test_get_tileset_info():
     assert(t['max_width'] > 4000000000)
     assert(t['max_width'] < 5000000000)
 
+
 def test_get_tiles():
     filename = 'test/sample_data/arrowhead_domains_short.txt.multires.db'
 
-    tiles = cdt.get_2d_tiles(filename, 0,0,0, numx=1, numy=1)
+    tiles = cdt.get_2d_tiles(filename, 0, 0, 0, numx=1, numy=1)
 
     #print("tiles:", tiles)
