a78af86c270c572d8cc934d82bd60cb4039a29e5
Nezar Abdennur <nabdennur@gmail.com>
2019-07-07 12:08:07 -0400

    Bump version: 0.10.12 → 0.10.13

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ install_requires = get_requirements('requirements.txt')
 
 setup(
     name='clodius',
-    version='0.10.12',
+    version='0.10.13',
     description='Tile generation for big data',
     author='Peter Kerpedjiev',
     author_email='pkerpedjiev@gmail.com',
