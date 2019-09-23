9b1b4f9758ea93662b5e74a95174c24c440cfda8
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2019-09-22 19:05:17 -0700

    Added bam and test files

--- get_test_data.sh
+++ get_test_data.sh
@@ -8,3 +8,7 @@ wget -q -NP data/ https://s3.amazonaws.com/pkerp/public/points_density.h5
 wget -q -NP data/ https://s3.amazonaws.com/pkerp/public/int_matrices.hdf5
 wget -q -NP data/ https://s3.amazonaws.com/pkerp/public/corrected.geneListwithStrand.bed.multires
 wget -q -NP data/ https://s3.amazonaws.com/pkerp/public/labels.h5
+wget -q -NP data/ https://s3.amazonaws.com/pkerp/public/SRR1770413.sorted.short.bam
+wget -q -NP data/ https://s3.amazonaws.com/pkerp/public/SRR1770413.different_index_filename.bai
+wget -q -NP data/ https://s3.amazonaws.com/pkerp/public/SRR1770413.sorted.short.bam.bai
+wget -q -NP data/ https://s3.amazonaws.com/pkerp/public/SRR1770413.mismatched_bai.bam
