dd4be5a2b83a1a50667b02899c4e7ea7cdc33225
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2020-01-16 11:55:43 -0800

    Updated get_test_data

--- get_test_data.sh
+++ get_test_data.sh
@@ -12,4 +12,5 @@ wget -q -NP data/ https://s3.amazonaws.com/pkerp/public/SRR1770413.sorted.short.
 wget -q -NP data/ https://s3.amazonaws.com/pkerp/public/SRR1770413.different_index_filename.bai
 wget -q -NP data/ https://s3.amazonaws.com/pkerp/public/SRR1770413.sorted.short.bam.bai
 wget -q -NP data/ https://s3.amazonaws.com/pkerp/public/SRR1770413.mismatched_bai.bam
+wget -q -NP data/ https://s3.amazonaws.com/pkerp/public/geneAnnotationsExonUnions.1000.bed.v3.beddb
 wget -q -NP data/ https://s3.amazonaws.com/areynolds/public/masterlist_DHSs_733samples_WM20180608_all_mean_signal_colorsMax.bed.bb
