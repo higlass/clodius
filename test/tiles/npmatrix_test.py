eab0c0bbb61f83165d833d705281a358a3f038e5
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2021-11-18 21:29:54 -0800

    Handle narrow matrices in clodius.tile.npmatrix

--- test/tiles/npmatrix_test.py
+++ test/tiles/npmatrix_test.py
@@ -1,10 +1,19 @@
-import clodius.tiles.npmatrix as hgnp
 import numpy as np
+
+import clodius.tiles.npmatrix as hgnp
 
 
 def test_numpy_matrix():
     grid = np.array(np.random.rand(100, 100))
-    # print('grid:', grid)
 
     tile = hgnp.tiles(grid, 0, 0, 0)
+    assert tile.shape == (256, 256)
+
+
+def test_numpy_narrow_matrix():
+    grid = np.array(np.random.rand(2, 10000))
+
+    # make sure we can fetch a tile that would be empty
+    # because of the narrowness of the matrix
+    tile = hgnp.tiles(grid, 1, 1, 0)
     assert tile.shape == (256, 256)
