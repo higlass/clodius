624fd7fc9cedde8246c45e65ed4a2f523f1ef074
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2025-04-06 15:56:53 -0700

    Test using file-like object

--- test/tiles/chromsizes_test.py
+++ test/tiles/chromsizes_test.py
@@ -7,8 +7,16 @@ from clodius.models.tileset_info import TilesetInfo
 def test_get_tileset_info():
     filename = op.join("data", "chromSizes.tsv")
 
+    # Test loading tileset info using a filename
     tsinfo = TilesetInfo(**ctcs.tileset_info(filename))
 
     assert tsinfo.max_width > 100
     assert len(tsinfo.chromsizes) > 2
     # TODO: Do something with the return value
+
+    with open(filename, "r") as f:
+        # Test loading using a file-like object
+        tsinfo = TilesetInfo(**ctcs.tileset_info(f))
+
+        assert tsinfo.max_width > 100
+        assert len(tsinfo.chromsizes) > 2
