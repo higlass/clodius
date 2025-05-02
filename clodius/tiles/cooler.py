f064711402af827328658dca265bde38fb854ad8
Nezar Abdennur <nabdennur@gmail.com>
2025-05-02 12:09:35 -0400

    Fix overflow on combining numpy and python numbers

--- clodius/tiles/cooler.py
+++ clodius/tiles/cooler.py
@@ -495,7 +495,7 @@ def make_mats(filepath):
 
         # get the genome size
         resolution = list(f["resolutions"].keys())[0]
-        genome_length = int(sum(f["resolutions"][resolution]["chroms"]["length"]))
+        genome_length = int(np.sum(f["resolutions"][resolution]["chroms"]["length"]))
 
         info["max_pos"] = [genome_length, genome_length]
         info["min_pos"] = [1, 1]
